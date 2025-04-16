ff8c29e4c4505fc98c9afd9edeb727bc19640fed
ISHIKA MANDAL <s223973381@deakin.edu.au>
2025-04-16 23:14:43 +1000

    Update and rename abc.txt to app.js

---
+++ Task6.2D/app.js
@@ -0,0 +1,25 @@
+
+const express = require('express');
+const app = express();
+const slotRoutes = require('./routes/slotRoutes');
+const path = require('path');
+require('dotenv').config();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', slotRoutes);
+
+const PORT = process.env.PORT || 3009;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+// Existing code
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+// Add this line for testing
+module.exports = app;
